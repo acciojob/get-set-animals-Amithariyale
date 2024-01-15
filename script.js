166f266187b926f1ee55b59896674d7af38eae33
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-01-15 22:35:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species=species;
+	}
+	get getSpecies(){
+		return this.species;
+	}
+	makeSound(){
+		console.log("Make Noice");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(){
+		super();
+	}
+	bark(){
+		console.log('woof');
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(){
+		super();
+	}
+
+	purr(){
+		console.log('purr');
+	}
+}
+
 
 // Do not change the code below this line
 window.Animal = Animal;
